135226ab23c6ff7de768e3f92147f73dc178f356
MensaSeven <91467101+Mensa-Seven@users.noreply.github.com>
2021-10-28 13:55:07 +0700

    Add files via upload

--- router/updateUser.js
+++ router/updateUser.js
@@ -7,25 +7,36 @@ router.put('/update/:id', async(req, res) => {
 
     try{
         Profile.findById({_id:req.params.id}, async(err, data) => {
-            
+
             if(data){
-                try{
-                    const updateUser = await Profile.findByIdAndUpdate({_id:data._id}, 
-                        {
-                            $set:req.body,
-                        },
-                        {
-                            $new:true
-                        }
-                        )
-                    res.status(200)
-                    .json(updateUser)
 
+                //auth
+                if(req.body.username === data.username){
+                    try{
+                        // update form body 
+                        const updateUser = await Profile.findByIdAndUpdate({_id:data._id}, 
+                            {
+                            
+                                $set:req.body,
+                            },
+                            {
+                                $new:true
+                            }
+                            )
+                        res.status(200)
+                        .json(updateUser)
+                        //return object all 
+    
+                    }catch(err){
+                        res.status(404)
+                        .json(err)
+                    }
 
-                }catch(err){
-                    res.status(400)
-                    .json(err)
+                }else{
+                    res.status(401)
+                    .json('you can update you profile only')
                 }
+                
             }
         })
 
--- router/users.js
+++ router/users.js
@@ -5,9 +5,11 @@ const Profile = require('../models/Profile')
 //get all user
 router.get('/', async(req, res) => {
     try{
+
         const allUser = await Profile.find()
         res.status(200)
         .json(allUser)
+
 
     }catch(err){
         res.status(200)
@@ -19,21 +21,26 @@ router.get('/', async(req, res) => {
 router.get('/me/:id', async(req, res) => {
 
     try{
-
-        //if(!req.params.id) return res.status(400).json("please defind header id")
+        
+        //check request body
         if(!req.body.username) return res.status(400).json("please defind username body")
+
+        //check resource user
         Profile.findById({_id:req.params.id}, async(err, data) => {
             if(data){
+
                 if(req.body.username === data.username){
                     res.status(200)
                     .json(data)
+                    //reutnr your object profile
+
                 }else{
                     res.status(401)
                     .json("you can get you accuct only")
                 }
                 
             }else{
-                res.status(400)
+                res.status(404)
                 .json(err)
             }
         }) 
